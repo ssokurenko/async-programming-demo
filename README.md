ca8cad027605f05ec2db1daa34a0adbe5f884ec6
Sergey Sokurenko <s.sokurenko@gmail.com>
2017-01-21 22:43:51 +0100

    Minor demo update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Asynch Programming
-A quick and simple demo app to illustrate asynchronous programming concepts
+A quick and simple demo to illustrate asynchronous programming concepts
 
 ## Local setup
 
@@ -9,3 +9,13 @@ Just run an HTTP server, for example:
 python -m SimpleHTTPServer 2222
 ```
 
+The server local server will be available at:
+
+http://localhost:2222
+
+## Demos
+
+### Promises
+
+A mini web app which randomly generates 100 products, the controls allow to search for an item by ID, show similar items, etc...
+[Demo link](https://ssokurenko.github.io/async-programming-demo/promises/).
--- promises/js/app.js
+++ promises/js/app.js
@@ -8,18 +8,21 @@ $(document).ready(function () {
     // DOM Elements
 
     app.dom = {
+        productProgressBar: $('.product-progress'),
         selectedProductID: $('.selected-product-id'),
         selectedProductPrice: $('.selected-product-price'),
         selectedProductType: $('.selected-product-type'),
         searchProductButton: $('button.search-product'),
         searchProductInput: $('#search-product-input'),
         similarProductsList: $('.similar-products-list'),
+        allProductsProgressBar: $('.all-products-progress'),
         allProductsList: $('.all-products-list')
     };
 
     // Events
 
     app.dom.searchProductButton.click(function () {
+        app.dom.productProgressBar.css('visibility', 'visible');
         api.searchProductById(app.dom.searchProductInput.val())
             .then(app.helpers.examineProduct)
             .catch(function (error) {
@@ -35,6 +38,7 @@ $(document).ready(function () {
 
     app.dom.promises.searchAllProducts
         .then(function (products) {
+            app.dom.allProductsProgressBar.hide();
             app.helpers.updateTable(app.dom.allProductsList, products);
         })
         .catch(function (error) {
@@ -47,11 +51,12 @@ $(document).ready(function () {
         updateExamineButtons: function () {
             app.dom.examineProductButtons = $('.examine-product-button');
             app.dom.examineProductButtons.click(function () {
-                api.searchProductById($(this).data('id'))
-                    .then(app.helpers.examineProduct);
+                app.dom.productProgressBar.css('visibility', 'visible');
+                api.searchProductById($(this).data('id')).then(app.helpers.examineProduct);
             });
         },
         examineProduct: function (product) {
+            app.dom.productProgressBar.css('visibility', 'hidden');
             app.dom.selectedProductID.html(product.id);
             app.dom.selectedProductPrice.html(product.price);
             app.dom.selectedProductType.html(product.type);
